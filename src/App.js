209102b7243412026715b8b03222159260355106
Pablo Hinojosa <pablo.hinojosa@ort.edu.ar>
2026-03-31 23:25:29 +0000

    subida inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo React en curso de ORT
         </p>
         <a
           className="App-link"
